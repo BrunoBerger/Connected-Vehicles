7d39cfe39e8183082f9fe80389c0f4e95f9b2a04
BrunoBerger <bruno.berger@stud.hshl.de>
2020-05-05 01:00:00 +0200

    mqtt and CARL-Interaction Tests

---
+++ carlaInteractionTest.py
@@ -0,0 +1,25 @@
+import glob
+import os
+import sys
+import random
+import time
+
+try:
+    sys.path.append(glob.glob('carla-*%d.%d-%s.egg' % (
+        sys.version_info.major,
+        sys.version_info.minor,
+        'win-amd64' if os.name == 'nt' else 'linux-x86_64'))[0])
+except IndexError:
+    pass
+
+import carla
+
+
+
+
+def main():
+
+
+if __name__ == '__main__':
+
+    main()
--- mqtt-Tests/TestClient1.py
+++ mqtt-Tests/TestClient1.py
@@ -1,19 +1,32 @@
 import paho.mqtt.client as mqtt
 import time
+import json
+
+pizza = "salami"
+quantity = 10
 
 def on_message(client, userdata, message):
     print("Just got this message: " ,str(message.payload.decode("utf-8")))
 
 
+data = {
+    "name": "Bruno",
+    "pizza": pizza,
+    "quantity": quantity,
+    "topic": "hshl/customer/myid",
+}
+
+
 client1 = mqtt.Client("myPC_testClient1")
 client1.connect("broker.mqttdashboard.com")
+
 print("Connected to HiveMQ-Server")
 
 client1.subscribe("city/car", qos=0)
-print("Subscribed to topic","city/car")
+print("Subscribed to topic", "city/car")
 
 client1.publish("city/car","HELLO THERE")
-client1.publish("city/car","BIG TEST HERE")
+client1.publish("city/car", json.dumps(data))
 
 client1.on_message=on_message
 
